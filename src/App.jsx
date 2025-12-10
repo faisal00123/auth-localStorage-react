30f9c34b429425e837047b9b96d903d43443450d
FAISAL FAYYAZ <faisalfayyaz500@gmail.com>
2025-12-10 20:09:07 +0500

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import SignUp from "./components/SignUp"
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
+import Login from './components/Login';
+
+const App = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<SignUp />} />
+        <Route path="/singup" element={<SignUp />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </Router>
+  )
+}
+
+export default App
